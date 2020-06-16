9ce6ddcabdd92b1ff75757aa6ec50a5c5f14dc34
Siddharth Kumar Singh <SidKumarSingh@users.noreply.github.com>
2020-06-16 15:49:33 +0530

    Updated 16/06/2020

--- data-scripts/get_global_ts.py
+++ data-scripts/get_global_ts.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 #####################################
 # Created on 02 May 2020            #
-# Last modified on 15 June 2020     #
+# Last modified on 16 June 2020     #
 #                                   #
 # @author: siddharth-kumar-singh    #
 #####################################
 # Changelog:                        
 # 10-Jun-2020: Added logic to aggregate provinces of countries
 # 15-Jun-2020: Added temporary code to adjust India data due to T-1 lag of JHU data
+# 16-Jun-2020: Removed temporary code added on 15-Jun
 #####################################
 # %%
 import pandas as pd 
@@ -25,21 +26,6 @@ def get_global_ts(ind_cnt):
     data_ts['Date'] = pd.to_datetime(data_ts['Date'],infer_datetime_format=True)
 
     data_ts = data_ts.groupby(['Country_Region','Date'],as_index=False).sum()
-
-    ### REMOVE THIS ONCE DATA FIXED IN JHU DATASET ###
-    # Correcting data in JHU
-    dt = datetime(2020,6,12)
-
-    ind_data = data_ts[(data_ts['Country_Region']=='India')&(data_ts['Date']>dt)].iloc[:,2]
-
-    for i in range(len(ind_data)):
-        data_ts.at[(data_ts['Country_Region']=='India')&(data_ts['Date']==dt),'Confirmed'] = ind_data.iat[i]
-        dt += timedelta(days=1)
-
-    data_ts.at[(data_ts['Country_Region']=='India')&(data_ts['Date']==dt),'Confirmed'] = ind_cnt
-    #print(data_ts[data_ts['Country_Region']=='India'])
-
-    ######
 
     return data_ts
 
